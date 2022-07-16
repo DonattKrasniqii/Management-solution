be397da386648c3295944149623476f55c6c96af
Donat <donatikr@gmail.com>
2022-07-16 23:51:19 +0200

    comment sector added

--- includes/navigation.php
+++ includes/navigation.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Start </a>
+            <a class="navbar-brand" href="index.php">Start </a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -25,7 +25,9 @@
                 echo "<li><a href='#'>{$cat_title}<a></li>";
             }
             ?>
-                <li><a href="admin">Admin Panel</a> </li>
+                <li>
+                    <a href="admin">Admin Panel</a>
+                </li>
 
 <!--                <li>-->
 <!--                    <a href="#">Services</a>-->
